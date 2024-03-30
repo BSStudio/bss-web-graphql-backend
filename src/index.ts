f9f82e57d5e33209167a2f0cb764d26e242ec502
Bence Csik <22349790+csikb@users.noreply.github.com>
2024-03-30 09:34:35 +0000

    update versions

--- src/index.ts
+++ src/index.ts
@@ -22,5 +22,5 @@ app
   .use(postGraphile)
   // start server
   .listen(config.port, () => {
-    console.log(`Server running on port :${config.port}`)
+    console.log(`Server running on port :${config.port.toString()}`)
   })
