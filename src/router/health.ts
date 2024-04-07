c5a021ce0408a2d5047e3fe2abb7cf16c073725e
Bence Csik <22349790+csikb@users.noreply.github.com>
2024-04-07 12:11:45 +0100

    Integration test (#28)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Integrated a new health check endpoint that queries the PostgreSQL
    database for views.
    - Introduced integration testing with Docker-compose environment setup
    for better reliability.
    - Added new scripts for integration testing in the development
    environment.
    - **Bug Fixes**
    - Updated health check commands for `frontend` and `backend` services to
    improve service monitoring.
    - **Documentation**
    - Updated comments to clarify the use of middleware and categorize
    dependencies.
    - **Tests**
    - Expanded test coverage to include health checks, GraphQL endpoint
    functionality, and ping functionality.
    - **Chores**
    - Enhanced ESLint configuration to include additional test and
    configuration files.
    - Modified Docker-compose configurations for improved service
    definitions and health checks.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- src/router/health.ts
+++ src/router/health.ts
@@ -1,11 +1,28 @@
 import Router from 'koa-router'
+import { postgres } from '../database/index.js'
 
 const router = new Router({
   prefix: '/actuator',
 })
 
-router.get('/health', (ctx) => {
-  ctx.body = 'OK'
+router.get('/health', async (ctx) => {
+  await postgres
+    .query(
+      "SELECT table_name FROM information_schema.views WHERE table_schema = 'public'",
+    )
+    .then(({ rows }) => {
+      if (rows.length >= 1) {
+        ctx.body = 'UP'
+        return
+      }
+      ctx.body = 'ERROR: no views found in database'
+      ctx.status = 500
+      return
+    })
+    .catch(() => {
+      ctx.body = 'ERROR: cannot connect to database'
+      ctx.status = 500
+    })
 })
 
 router.get('/ping', (ctx) => {
