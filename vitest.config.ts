ed74b071afa888b043dda1dda6b37ae8bb265757
Bence Csik <22349790+csikb@users.noreply.github.com>
2024-04-07 13:33:17 +0100

    fix coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['tests/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    coverage: {
+      include: ['src/**'],
+    },
   },
 })
