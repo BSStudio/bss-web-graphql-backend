1907307dda59347190562498110c77480f4bdaaa
Bence Csik <22349790+csikb@users.noreply.github.com>
2024-04-10 21:09:35 +0100

    wip

--- src/config.ts
+++ src/config.ts
@@ -2,14 +2,14 @@ import dotenv from 'dotenv'
 import { PostGraphileOptions } from 'postgraphile'
 import { PoolConfig } from 'pg'
 
-dotenv.config()
-
 export interface Config {
   port: number
   database: PoolConfig
   schema: string
   postGraphile: PostGraphileOptions
 }
+
+dotenv.configDotenv()
 
 if (process.env.DATABASE_CONNECTION_STRING === undefined) {
   throw new Error('DATABASE_CONNECTION_STRING is not set')
@@ -23,6 +23,7 @@ const config: Config = {
   },
   schema: process.env.DATABASE_SCHEMA || 'public',
   postGraphile: {
+    allowExplain: process.env.POSTGRAPHILE_ALLOW_EXPLAIN === 'true',
     graphiql: process.env.POSTGRAPHILE_GRAPHIQL === 'true',
     watchPg: process.env.POSTGRAPHILE_WATCH_PG === 'true',
     showErrorStack: process.env.POSTGRAPHILE_SHOW_ERROR_STACK === 'true',
