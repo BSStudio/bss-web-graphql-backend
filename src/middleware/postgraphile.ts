1aead7c7a53f738fd5bb1e2060215b34c8d6f712
Bence Csik <22349790+csikb@users.noreply.github.com>
2025-12-08 08:03:03 +0000

    fix: update PostGraphile plugin import to use default export

--- src/middleware/postgraphile.ts
+++ src/middleware/postgraphile.ts
@@ -12,7 +12,7 @@ const options: PostGraphileOptions = {
   setofFunctionsContainNulls: false,
   ignoreRBAC: false,
   extendedErrors: ['errcode'],
-  appendPlugins: [PgSimplifyInflectorPlugin],
+  appendPlugins: [PgSimplifyInflectorPlugin.default],
   graphiql: false,
   enableQueryBatching: true,
   disableQueryLog: true,
